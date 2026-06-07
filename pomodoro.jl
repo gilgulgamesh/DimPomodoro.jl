5e470a24c5bcc6d2bd382d1b029020f5924f262d
gilgulgalgal <gilgulgalgal@gmail.com>
2026-06-07 23:58:00 +1300

    hello world

---
+++ pomodoro.jl
@@ -0,0 +1,131 @@
+artifact_bin = joinpath(first(filter(d -> isdir(joinpath(d, "qml", "Qt", "labs", "platform")), 
+	readdir(joinpath(first(Base.DEPOT_PATH), "artifacts"), join=true))), "bin")
+sep = Sys.iswindows() ? ";" : ":"
+ENV["PATH"] = artifact_bin * sep * ENV["PATH"]
+
+
+using QML
+using Observables	
+
+#vars
+qml_file = "pomodoro.qml"
+resttime = 5*60
+bigresttime = 15*60
+gotime = 30*60
+gocolor = "#ffc135" 
+restcolor = "#a6e22d"
+bigrestcolor = "#ce8297"
+ncorner = 1
+margin = 10
+
+gray = "#d0c8aa"
+yellow = "#fabd2f"
+green = "#b8bb26"
+blue = "#96a89e"
+purple = "#bf93a0"
+teal = "#8ec07c"
+white = "#e5debb"
+red = "#fb4934"
+orange = "#fe8019"
+# opacity is in the qml
+
+#corners! 
+left = margin
+right = 1920  - margin - 200
+top = margin
+bottom = 1080  - margin - 100
+nw = (left, top)
+ne = (right, top)
+sw = (left, bottom)
+se = (right, bottom)
+corners = (nw, ne, se, sw)  # hard code your resolution above to be able to start anywhere that isn't (0, 0)
+
+const timestring = Observable("11:11:11")
+const visible = Observable(true)
+const x = Observable(left) # see below
+const y = Observable(top)
+const t = Observable(gotime)
+const color = Observable(gocolor)
+const inrest = Observable(false)
+const paused = Observable(false)
+
+
+
+function tocorner!(n)
+	rand1 = trunc(Int, 40rand())
+	rand2 = trunc(Int, 40rand())
+	(x[], y[]) = corners[n] .+ (rand1, rand2)
+end
+
+on(paused) do p
+	if p 
+		visible[] = true
+	end
+end
+
+on(inrest) do inr
+	if inr
+		if ncorner == 4 
+			t[]  = bigresttime
+			color[] = bigrestcolor
+		else
+			t[] = resttime
+			color[] = restcolor
+		end 
+		paused[] = true
+		println("next break")
+	else
+		t[] = gotime
+		color[] = gocolor
+		global ncorner
+		ncorner = ncorner %4 + 1 # looks odd, but after 1-indexing it's normal
+		tocorner!(ncorner)
+		println("next pomo")
+	end
+	timestring[] = formattime(t[])
+
+end
+
+#time!
+
+function formattime(timen) 
+	sec = timen % 60 
+	min =  timen ÷ 60  
+	if min < 10
+		min = string(0, min)
+	end
+	if sec < 10
+		sec = string(0, sec) 
+	end
+	string(min, ":", sec)
+end
+
+backtocorner() = tocorner!(ncorner)
+skip() = inrest[] = !inrest[]
+playpause() = paused[] = !paused[]
+
+@qmlfunction println backtocorner skip playpause
+engine = loadqml(qml_file, pomo = JuliaPropertyMap("paused" => paused, "color" => color, "x" => x, "y" => y, "timestring"=>timestring, "visible" => visible))
+# QML.watchqml(engine, qml_file)
+
+trunning  = false
+timekeeper = @async begin
+while true
+	@async while !paused[] && !trunning
+	global trunning
+	trunning = true
+	timestring[] = formattime(t[])
+	t[] -= 1
+	sleep(1)
+	if t[]  ≤  0
+		inrest[] = !inrest[] # triggers all other things
+	end
+	trunning = false
+end
+sleep(0.01)
+end
+end
+
+
+
+exec_async()
---
+++ pomodoro.qml
@@ -0,0 +1,125 @@
+import QtQuick
+import QtQuick.Controls 6.0
+import jlqml
+import QtQuick.Window
+import Qt.labs.platform
+
+
+
+ApplicationWindow {
+	id: window
+	visible: pomo.visible
+	flags: Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint 
+	color: "transparent"
+	width: 200
+	height: 64
+	x: pomo.x
+	y: pomo.y
+	title: "pomo"
+
+	// Shortcut {
+	// 	sequence: "F10"
+	// 	context: Qt.ApplicationShortcut
+	// 	onActivated:  pomo.visible = !pomo.visible
+	// }
+
+	SystemTrayIcon {
+		visible: true
+		icon.source: Qt.resolvedUrl("icon.png")
+
+		onActivated: pomo.visible = !pomo.visible
+		Component.onCompleted: {
+			show()
+		}
+
+	}
+
+
+	Text {
+		opacity: 0.85
+		id:text
+		anchors.centerIn: parent
+		text: pomo.timestring
+		font.pixelSize: 40
+		color: pomo.color
+		
+		SequentialAnimation on opacity {
+			running : false
+			id : animateToBright
+			NumberAnimation{  to: 1.0; duration: 0}
+
+		}
+		SequentialAnimation on opacity {
+			id : animateFromBright
+			running : false
+			NumberAnimation { to:0.7; duration:100 }
+		}
+		SequentialAnimation on opacity {
+			id : animateFromBrightPause
+			running : false
+			NumberAnimation { to: 0.2 ; duration: 100 }
+			PauseAnimation { duration: 600 }
+		}
+		SequentialAnimation on opacity {
+			id : animateFromBrightPlay
+			running : false
+			NumberAnimation { to:0.7; duration:200 }
+			PauseAnimation {duration: 200}
+		}
+
+
+		SequentialAnimation  on opacity{
+			id : animateBlink
+			loops: Animation.Infinite
+			running : pomo.paused && !animateFromBrightPlay.running && !animateFromBrightPause.running
+			NumberAnimation {  to:0.2 ; duration:70}
+			NumberAnimation {  to:0.7 ; duration:2000}
+			NumberAnimation {  to:0.2 ; duration:2000}
+
+		}
+
+		
+	}
+
+	MouseArea {
+		anchors.fill: parent
+		acceptedButtons: Qt.RightButton | Qt.MiddleButton | Qt.LeftButton
+		onPressed: mouse => {
+			if (mouse.button === Qt.LeftButton) {
+				window.startSystemMove() ;
+				pomo.x = window.x;pomo.y = window.y
+			} else {
+				animateToBright.start()
+			}
+
+		}
+
+		onReleased: mouse => {
+			if (mouse.button !== Qt.leftButton) {
+
+			}
+			if (mouse.button === Qt.RightButton) {
+				if (pomo.paused) {
+					animateFromBrightPlay.start()
+				} else {
+					animateFromBrightPause.start()
+				}
+				Julia.playpause()
+			}
+			if (mouse.button === Qt.MiddleButton) {
+				animateFromBright.start()
+				Julia.backtocorner()
+			}
+
+		}
+		onDoubleClicked: mouse => {
+			if (mouse.button === Qt.LeftButton) {
+				Julia.skip()
+			}
+		}
+	}
+
+	Component.onCompleted:  Julia.println("ready")
+	// Component.onCompleted: console.log(Qt.version)
+
+}
