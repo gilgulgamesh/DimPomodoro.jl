2fdfc1a11e1a117a2a56388c30915cfe9f494a6d
gilgulgalgal <gilgulgalgal@gmail.com>
2026-06-29 22:02:45 +1300

    now it fills my screen too, instead of turning it off too

--- pomodoro.jl
+++ pomodoro.jl
@@ -1,9 +1,9 @@
 println("launching")
 
-artifact_bin = joinpath(first(filter(d -> isdir(joinpath(d, "qml", "Qt", "labs", "platform")),
-        readdir(joinpath(first(Base.DEPOT_PATH), "artifacts"), join=true))), "bin")
-sep = Sys.iswindows() ? ";" : ":"
-ENV["PATH"] = artifact_bin * sep * ENV["PATH"]
+# artifact_bin = joinpath(first(filter(d -> isdir(joinpath(d, "qml", "Qt", "labs", "platform")),
+#         readdir(joinpath(first(Base.DEPOT_PATH), "artifacts"), join=true))), "bin")
+# sep = Sys.iswindows() ? ";" : ":"
+# ENV["PATH"] = artifact_bin * sep * ENV["PATH"]
 
 using QML
 using Observables
@@ -20,16 +20,16 @@ orange = "#fe8019"
 
 #vars
 qml_file = "pomodoro.qml"
-resttime = 5 * 60
+resttime = 2 * 60
 bigresttime = 60 * 60
-gotime = 25 * 60
+gotime = 15 * 60
 gocolor = orange
 restcolor = teal
 bigrestcolor = purple
 ncorner = 1
 margin = 10
 SCALING_FACTOR = 1 #set this to your ui scaling factor, or just play with it
-# opacity is in the qml
+
 
 #corners!
 left = margin
@@ -52,8 +52,32 @@ const t = Observable(gotime)
 const color = Observable(gocolor)
 const inrest = Observable(false)
 const paused = Observable(false)
+const width = Observable(200)
+const height = Observable(64)
+const windowcolor = Observable("transparent")
+const freeze = Observable(false)
+const opacity = Observable(0.75)
+const font = Observable("Meslo")
+const fontsize = Observable(26)
 
 
+on(freeze) do f
+    if f
+        rand1 = trunc(Int, 200rand())
+        rand2 = trunc(Int, 200rand())
+        x[] = y[] = 0
+        width[] = 1920 + rand1
+        height[] = 1080 + rand2
+        windowcolor[] = "#99000000"
+        opacity[] = 0.5
+    else
+        backtocorner()
+        opacity[] = 0.75
+        width[] = 200
+        height[] = 64
+        windowcolor[] = "transparent"
+    end
+end
 
 function tocorner!(n)
     rand1 = trunc(Int, 40rand())
@@ -63,22 +87,32 @@ end
 
 on(paused) do p
     if p
+        freeze[] = true
         visible[] = true
+    else
+        if !inrest[]
+            freeze[] = false
+        end
+        t[] = t[] - 1
     end
+
 end
 
 on(inrest) do inr
     if inr
+        freeze[] = true
         if ncorner == 4
             t[] = bigresttime
             color[] = bigrestcolor
         else
             t[] = resttime
             color[] = restcolor
+
         end
-        paused[] = true
+        # paused[] = true
         println("next break")
     else
+        freeze[] = false
         t[] = gotime
         color[] = gocolor
         global ncorner
@@ -86,6 +120,8 @@ on(inrest) do inr
         tocorner!(ncorner)
         println("next pomo")
     end
+    paused[] = true
+
     timestring[] = formattime(t[])
 
 end
@@ -112,7 +148,7 @@ skip() = inrest[] = !inrest[]
 playpause() = paused[] = !paused[]
 
 @qmlfunction println backtocorner skip playpause
-engine = loadqml(qml_file, pomo=JuliaPropertyMap("paused" => paused, "color" => color, "x" => x, "y" => y, "timestring" => timestring, "visible" => visible))
+engine = loadqml(qml_file, pomo=JuliaPropertyMap( "width" => width, "height" => height, "windowcolor" => windowcolor, "color" => color, "x" => x, "y" => y, "timestring" => timestring, "visible" => visible, "paused" => paused, "inrest" => inrest, "freeze" => freeze, "opacity" => opacity, "font" => font, "fontsize" => fontsize))
 # QML.watchqml(engine, qml_file)
 
 trunning = false
@@ -136,3 +172,20 @@ end
 
 
 exec_async()
+
+mytime() = begin
+    seconds = (time()÷1) % 60
+    minutes = (time()÷60) % 60
+    hours = (time()÷(60*60)) % 12 + 1
+    return Int.([hours minutes seconds])
+end
+
+# if mytime()[1] == 5
+#     if mytime()[2] == 0
+#         freeze[] = true
+#     else
+#         if mytime()[2] == medlength
+#         freeze[] = false
+#         end
+#     end
+# end
--- pomodoro.qml
+++ pomodoro.qml
@@ -10,9 +10,9 @@ ApplicationWindow {
 	id: window
 	visible: pomo.visible
 	flags: Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint
-	color: "transparent"
-	width: 200
-	height: 64
+	color: pomo.windowcolor
+	width:  pomo.width
+	height: pomo.height
 	x: pomo.x
 	y: pomo.y
 	title: "pomo"
@@ -36,47 +36,48 @@ ApplicationWindow {
 
 
 	Text {
-		opacity: 0.8
+		opacity: pomo.opacity
 		id:text
 		anchors.centerIn: parent
 		text: pomo.timestring
-		font.pixelSize: 40
+		font.pointSize: pomo.fontsize
+		font.family: pomo.font
 		color: pomo.color
 
-       	SequentialAnimation on opacity {
-			running : false
-			id : animateToBright
-			NumberAnimation{  to: 1.0; duration: 0}
+  //      	SequentialAnimation on opacity {
+		// 	running : false
+		// 	id : animateToBright
+		// 	NumberAnimation{  to: 0.85; duration: 0}
 
-		}
-		SequentialAnimation on opacity {
-			id : animateFromBright
-			running : false
-			NumberAnimation { to:0.8; duration:100 }
-		}
-		SequentialAnimation on opacity {
-			id : animateFromBrightPause
-			running : false
-			NumberAnimation { to: 0.2 ; duration: 100 }
-			PauseAnimation { duration: 600 }
-		}
-		SequentialAnimation on opacity {
-			id : animateFromBrightPlay
-			running : false
-			NumberAnimation { to:0.8; duration:200 }
-			PauseAnimation {duration: 200}
-		}
+		// }
+		// SequentialAnimation on opacity {
+		// 	id : animateFromBright
+		// 	running : false
+		// 	NumberAnimation { to:0.75; duration:100 }
+		// }
+		// SequentialAnimation on opacity {
+		// 	id : animateFromBrightPause
+		// 	running : false
+		// 	NumberAnimation { to: 0.2 ; duration: 100 }
+		// 	PauseAnimation { duration: 600 }
+		// }
+		// SequentialAnimation on opacity {
+		// 	id : animateFromBrightPlay
+		// 	running : false
+		// 	NumberAnimation { to:0.75; duration:200 }
+		// 	PauseAnimation {duration: 200}
+		// }
 
 
-		SequentialAnimation  on opacity{
-			id : animateBlink
-			loops: Animation.Infinite
-			running : pomo.paused && !animateFromBrightPlay.running && !animateFromBrightPause.running
-			NumberAnimation {  to:0.2 ; duration:70}
-			NumberAnimation {  to:0.8 ; duration:2000}
-			NumberAnimation {  to:0.2 ; duration:2000}
+		// SequentialAnimation  on opacity{
+		// 	id : animateBlink
+		// 	loops: Animation.Infinite
+		// 	running : pomo.paused // && !animateFromBrightPlay.running && !animateFromBrightPause.running
+		// 	NumberAnimation {  to:0.2 ; duration:70}
+		// 	NumberAnimation {  to:0.5 ; duration:2000}
+		// 	NumberAnimation {  to:0.2 ; duration:2000}
 
-		}
+		// }
 
 
 	}
@@ -85,11 +86,11 @@ ApplicationWindow {
 		anchors.fill: parent
 		acceptedButtons: Qt.RightButton | Qt.MiddleButton | Qt.LeftButton
 		onPressed: mouse => {
-			if (mouse.button === Qt.LeftButton) {
+			if (mouse.button === Qt.LeftButton && !pomo.freeze) {
 				window.startSystemMove() ;
 				pomo.x = window.x;pomo.y = window.y
 			} else {
-				animateToBright.start()
+				// animateToBright.start()
 			}
 
 		}
@@ -99,15 +100,15 @@ ApplicationWindow {
 
 			}
 			if (mouse.button === Qt.RightButton) {
-				if (pomo.paused) {
-					animateFromBrightPlay.start()
-				} else {
-					animateFromBrightPause.start()
-				}
+				// if (pomo.paused) {
+				// 	animateFromBrightPlay.start()
+				// } else {
+				// 	animateFromBrightPause.start()
+				// }
 				Julia.playpause()
 			}
-			if (mouse.button === Qt.MiddleButton) {
-				animateFromBright.start()
+			if (mouse.button === Qt.MiddleButton && !pomo.freeze) {
+				// animateFromBright.start()
 				Julia.backtocorner()
 			}
 
